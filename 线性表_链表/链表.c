1e80a17d3a305c3f10da8fdb0d51960520234457
conan <gosicklihai@proton.com>
2021-04-05 18:07:45 +0800

    添加 合并两个表函数

--- 线性表_链表/链表.c
+++ 线性表_链表/链表.c
@@ -2,42 +2,43 @@
 #include <malloc.h>
 
 #define Elemtype int
-#define Status int          //状态 状态类型为int型
-#define ElemType int        //用户定义类型
-#define TRUE 1              
+#define Status int   //状态 状态类型为int型
+#define ElemType int //用户定义类型
+#define TRUE 1
 #define FALSE 0
 #define ERROR -1
 
-
-
-typedef struct LNode 
+typedef struct LNode
 {
     Elemtype data;
     struct LNode *next;
-}LNode,*LinkList;
+} LNode, *LinkList;
 
 LinkList L;
+LinkList L2;
 
 /*
     初始化链表
 */
-Status Init_L(){
+Status Init_L(LinkList *L)
+{
     //初始化链表就是创建一个头节点，头节点的指针域的指针指向空
-    if (L = (LNode *)malloc(sizeof(LNode)))
+    if (*L = (LNode *)malloc(sizeof(LNode)))
     {
-        L->next = NULL;
-        L->data = 0;   //头节点的data域存放链表的长度
+        (*L)->next = NULL;
+        (*L)->data = 0; //头节点的data域存放链表的长度
         return TRUE;
     }
-    else return ERROR;
+    else
+        return ERROR;
 }
-
 
 /*
     按值查找，只查找第一个匹配的节点
     @t  放回要查找的节点的位置
     @Q  返回要查找的节点的地址
 */
+/*
 Status ListSelectValues_L(LinkList L, ElemType e,int *t,LinkList *Q){
     LinkList P = L->next;   //直接指向第一个节点
     int i = 1;              //不检查头节点，防止发生错误                  
@@ -54,11 +55,13 @@ Status ListSelectValues_L(LinkList L, ElemType e,int *t,LinkList *Q){
     *Q = P;             //将找到节点的地址返回给传递过来的结构体指针变量
     return TRUE;
 }
+*/
 
-int ListSelectValues_L(LinkList L, ElemType e){
-    LinkList P = L->next;   //直接指向首节点
-    int i = 1;              //不检查头节点，防止发生错误                  
-    while (P && (P->data!=e))           //链表没到头和没找到值
+int ListSelectValues_L(LinkList L, ElemType e)
+{
+    LinkList P = L->next;       //直接指向首节点
+    int i = 1;                  //不检查头节点，防止发生错误
+    while (P && (P->data != e)) //链表没到头和没找到值
     {
         P = P->next;
         i++;
@@ -70,15 +73,14 @@ int ListSelectValues_L(LinkList L, ElemType e){
     return i;
 }
 
-
-
-Status GetElem_L(LNode *L,int i,ElemType *e){
+Status GetElem_L(LNode *L, int i, ElemType *e)
+{
     LNode *p = L;
-    int j=0;        //计数器,0 代表指向头指针
-    while (p && j<i)
+    int j = 0; //计数器,0 代表指向头指针
+    while (p && j < i)
     {
-        p = p->next;    //将指针p移向下一个结点         
-        j++;            //计数器加一
+        p = p->next; //将指针p移向下一个结点
+        j++;         //计数器加一
     }
     if (!p || i > j)
     {
@@ -88,7 +90,6 @@ Status GetElem_L(LNode *L,int i,ElemType *e){
     return TRUE;
 }
 
-
 /*
     j               
     ^
@@ -96,10 +97,11 @@ Status GetElem_L(LNode *L,int i,ElemType *e){
     |   |   |   |   |
 */
 
-Status ListInsert_L(LNode *L,int i,ElemType e){
-    LNode *p = L,*s;
-    int j = 0;          //查找节点的指针
-    while (p && j < i - 1)      //找到
+Status ListInsert_L(LNode *L, int i, ElemType e)
+{
+    LNode *p = L, *s;
+    int j = 0;             //查找节点的指针
+    while (p && j < i - 1) //找到
     {
         p = p->next;
         j++;
@@ -111,9 +113,10 @@ Status ListInsert_L(LNode *L,int i,ElemType e){
         s->next = p->next;
         p->next = s;
         s->data = e;
-        L->data++;  // 链表长度加一
+        L->data++; // 链表长度加一
         return TRUE;
-    }else
+    }
+    else
     {
         return ERROR;
     }
@@ -122,15 +125,16 @@ Status ListInsert_L(LNode *L,int i,ElemType e){
 /*
     插入数据
 */
-Status ListInsertData_L(LinkList L,int i,ElemType e){
+Status ListInsertData_L(LinkList L, int i, ElemType e)
+{
     LinkList P = L;
     int j = 0;
-    while (P&&j<i)
+    while (P && j < i)
     {
         P = P->next;
         j++;
     }
-    if (!P||j>i)
+    if (!P || j > i)
     {
         return ERROR;
     }
@@ -138,24 +142,24 @@ Status ListInsertData_L(LinkList L,int i,ElemType e){
     return TRUE;
 }
 
-
-Status ListDelete_L(LNode *L,int i,ElemType *e){
-    LNode *p = L,*s;
+Status ListDelete_L(LNode *L, int i, ElemType *e)
+{
+    LNode *p = L, *s;
     int j = 0;
     while (p && j < i - 1)
     {
         p = p->next;
         j++;
     }
-    if (!p || j > i-1)
+    if (!p || j > i - 1)
     {
         return ERROR;
     }
-    s = p->next;        //暂时保留要删除节点的指针
+    s = p->next; //暂时保留要删除节点的指针
     p->next = s->next;
     *e = s->data;
     free(s);
-    L->data--;  // 链表长度减一
+    L->data--; // 链表长度减一
     return TRUE;
     /*
     e = (p->next)->data;            //将要删除的数据返回给e
@@ -163,25 +167,27 @@ Status ListDelete_L(LNode *L,int i,ElemType *e){
     free(); */
 }
 
-void ListDisplay_L(LNode *L){
+void ListDisplay_L(LNode *L)
+{
     LNode *p = L;
     while (p)
     {
-        printf("%d\t",p->data);
-        p = p->next; 
+        printf("%d\t", p->data);
+        p = p->next;
     }
 }
 
 /*
     获取链表长度（头节点为0）
 */
-void ListLength_L(LNode *L,int *e){
+void ListLength_L(LNode *L, int *e)
+{
     LNode *p = L->next;
     int i = 0;
-    while (p != NULL)           //假如指针指向的节点不为空计数器加一
+    while (p != NULL) //假如指针指向的节点不为空计数器加一
     {
         i++;
-        p = p->next;   
+        p = p->next;
     }
     *e = i;
 }
@@ -189,8 +195,9 @@ void ListLength_L(LNode *L,int *e){
 /*
     尾插法建立链表
 */
-void CreateList_L(LinkList L,int n){
-    LinkList P,Q;
+void CreateList_L(LinkList L, int n)
+{
+    LinkList P, Q;
     int i;
     P = L;
     for (i = 0; i < n; i++)
@@ -202,11 +209,11 @@ void CreateList_L(LinkList L,int n){
     }
 }
 
-
 /*
     头插法建立链表
 */
-void CreateListHead_L(LinkList L,int n){
+void CreateListHead_L(LinkList L, int n)
+{
     LinkList P;
     int i;
     for (i = 0; i < n; i++)
@@ -217,48 +224,78 @@ void CreateListHead_L(LinkList L,int n){
     }
 }
 
+/*
+    合并两个链表
+*/
+void MergeList_e(LinkList L1, LinkList L2, LinkList *L3)
+{
+    LinkList PA, PB, PC;
+    PA = L1->next;
+    PB = L2->next;
+    PC = *L3 = L1;
+    while (PA && PB)
+    {
+        if (PA->data < PB->data)
+        {
+            PC->next = PA;
+            PC = PA;
+            PA = PA->next;
+        }
+        else
+        {
+            PC->next = PB;
+            PC = PB;
+            PB = PB->next;
+        }
+    }
+    PC->next = (PA ? PA : PB);
+    free(L2);
+}
 
-void main(){
-//    LNode *L;
+void main()
+{
+    //    LNode *L;
     LinkList K;
-    int i,e;
-    Init_L();
-    ListLength_L(L,&i);
-    CreateListHead_L(L,10);
-//    CreateList_L(L,10);
+    int i;
+    Init_L(&L);
+    ListLength_L(L, &i);
+    CreateListHead_L(L, 10);
+    //    CreateList_L(L,10);
     for (i = 0; i < 10; i++)
-    {   
-        if (ListInsertData_L(L,i+1,i+1) != 1)
+    {
+        if (ListInsertData_L(L, i + 1, i * i) != 1)
         {
             printf("插入错误\n");
         }
     }
-    if (ListInsertData_L(L,11,10) != TRUE)
+    ListLength_L(L, &i);
+    printf("链表的长度为:%d\n", i);
+    printf("头节点(其数据域存储链表的长度)\n|\nV\n");
+    ListDisplay_L(L);
+    printf("\n");
+    // ListDelete_L(L,3,&e);
+    // printf("要删除的数据是:%d\n",e);
+    // printf("头节点(其数据域存储链表的长度)\n|\nV\n");
+    // ListDisplay_L(L);
+    // printf("\n");
+    Init_L(&L2);
+    CreateListHead_L(L2, 10);
+    ListLength_L(L2, &i);
+    printf("链表的长度为:%d\n", i);
+    //    CreateList_L(L,10);
+    for (i = 0; i < 10; i++)
     {
-        printf("插入错误\n");
+        if (ListInsertData_L(L2, i + 1, i + 1) != 1)
+        {
+            printf("插入错误\n");
+        }
     }
-    
-    // printf("链表的长度为:%d\n",i);
-    // for (i = 1; i < 10; i++)
-    // {
-    //     if (!ListInsert_L(L,i,i))
-    //     {
-    //         printf("插入失败");
-    //     }
-    // }
-    ListLength_L(L,&i);
-    printf("链表的长度为:%d\n",i);
-    printf("头节点(其数据域存储链表的长度)\n|\nV\n");
-    ListDisplay_L(L);
+    // printf("test");
+    ListDisplay_L(L2);
     printf("\n");
-    ListDelete_L(L,3,&e);
-    printf("要删除的数据是:%d\n",e);
-    printf("头节点(其数据域存储链表的长度)\n|\nV\n");
-    ListDisplay_L(L);
+    MergeList_e(L, L2, &K);
+    ListDisplay_L(K);
     printf("\n");
-    if(!ListSelectValues_L(L,5,&e,&K)){
-        printf("链表内没有这个数据!\n");
-    }else{
-        printf("查找数据在链表中的位置:%d\n找到节点的数据:%d\n要查找的数据值:5\n",e,K->data);
-    }
+    ListLength_L(K, &i);
+    printf("链表的长度为:%d\n", i);
 }
